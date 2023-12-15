6053e54e0549706e6b90b1d7eac4847d19313847
k022g0055@g.neec.ac.jp <k022g0055@g.neec.ac.jp>
2023-12-15 17:13:18 +0900

    円の拡縮とプレイヤーの移動

--- Assets/Script/Circle.cs
+++ Assets/Script/Circle.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class Circle : MonoBehaviour
 {
+    new Transform transform;
+
+    private Vector2 circleSize;
+
+    public float circleSizeupSpeed;
+    public Vector2 standardCircle;
+
+
+    private void CircleControll()
+    {
+        CircleSizeControll();
+    }
+    private void CircleSizeControll()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            circleSize = new Vector2(circleSize.x + circleSizeupSpeed, circleSize.y + circleSizeupSpeed);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            circleSize = new Vector2(circleSize.x - circleSizeupSpeed, circleSize.y - circleSizeupSpeed);
+        }
+        CircleScale(circleSize);
+    }
+
+    private void CircleScale(Vector2 scale)
+    {
+        transform.localScale = new Vector3(scale.x, 1, scale.y);
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform = GetComponent<Transform>();
+
+        circleSize = standardCircle;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CircleControll();
     }
 }
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Rigidbody rb;
+
+    public float playerMoveAcce;
+    public float playerMoveSpeedMax;
+
+    private void PlayerControll()
+    {
+        PlayerMove();
+    }
+    private void PlayerMove()
+    {
+        PlayerAddSpeed();
+    }
+    private void PlayerAddSpeed()
+    {
+        if (Input.GetAxis("leftStickX") != 0 || Input.GetAxis("leftStickY") != 0)
+        {
+            //rb.velocity = Vector3.zero;
+            rb.velocity = new Vector3(rb.velocity.x + playerMoveAcce * Input.GetAxis("leftStickX"), 0, rb.velocity.z - playerMoveAcce * Input.GetAxis("leftStickY"));
+        }
+      
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        PlayerControll();
     }
 }
