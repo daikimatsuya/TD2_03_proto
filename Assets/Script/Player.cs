3d9fe3d11b79d15116ec6aaeeb6f3661ec8efe66
k022g0055 <k022g0055@g.neec.ac.jp>
2023-12-20 11:48:23 +0900

    プレイヤーの移動大体できた

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -7,9 +7,11 @@ public class Player : MonoBehaviour
 {
     Rigidbody rb;
     private Transform cameraPos;
+    public GameObject camera;
 
     public float playerMoveAcce;
     public float playerMoveSpeedMax;
+    public float cameraRotate;
 
     private void PlayerControll()
     {
@@ -26,8 +28,8 @@ public class Player : MonoBehaviour
             rb.velocity = Vector3.zero;
             Vector2 stick = new Vector2(playerMoveAcce * Input.GetAxis("leftStickX"), -playerMoveAcce * Input.GetAxis("leftStickY"));
             Vector2 camera = CameraVectorTransform(stick);
-
-            rb.velocity = new Vector3(rb.velocity.x + camera.y, 0, rb.velocity.z + camera.x);
+            //rb.velocity = new Vector3(rb.velocity.x + stick.x, 0, rb.velocity.z +stick.y);
+            rb.velocity = new Vector3(rb.velocity.x + camera.x, 0, rb.velocity.z + camera.y);
         }
         else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A)||Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.S))
         {
@@ -84,23 +86,33 @@ public class Player : MonoBehaviour
     {
         Vector2 vector;
         vector = Vector2.zero;
-        vector = new Vector2(move.x * (float)Math.Cos(cameraPos.rotation.y) + move.y * (float)Math.Sin(cameraPos.rotation.y), move.x * (float)Math.Sin(cameraPos.rotation.y) + move.y * (float)Math.Cos(cameraPos.rotation.y));
+        vector = new Vector2(vector.x + move.x * camera.transform.right.x, vector.y + move.x * camera.transform.right.z);
+        vector = new Vector2(vector.x + move.y * CameraForward().x, vector.y + move.y * CameraForward().y);
         return vector;
     }
     public float ToRadian(float angle)
     {
         return angle * (float)Math.PI / 180f;
     }
+    public Vector2 CameraForward()
+    {
+        Vector2 forward = Vector2.zero;
+        forward = Vector2.Scale(new Vector2(camera.transform.forward.x, camera.transform.forward.z), new Vector2(1, 1).normalized);
+        return forward;
+    }
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         cameraPos=GameObject.FindWithTag("MainCamera").GetComponent<Transform>();
+        //camera = GetComponent<GameObject>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         PlayerControll();
+        cameraRotate = cameraPos.localRotation.y;
     }
 }
